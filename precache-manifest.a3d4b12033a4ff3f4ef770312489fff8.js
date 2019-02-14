d766c32429410972ad8bf495e4bc3dd4a01dd354
Regina <regina.zaripova@gmail.com>
2019-02-14 14:06:57 -0500

    Updates

---
+++ precache-manifest.a3d4b12033a4ff3f4ef770312489fff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "569b2b33e303e431ba73",
+    "url": "/myapp/static/js/runtime~main.569b2b33.js"
+  },
+  {
+    "revision": "4ab682030da391f63191",
+    "url": "/myapp/static/js/main.4ab68203.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/myapp/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "4ab682030da391f63191",
+    "url": "/myapp/static/css/main.3aea279b.chunk.css"
+  },
+  {
+    "revision": "8ea0e870993396cea8e80cf026432bca",
+    "url": "/myapp/index.html"
+  }
+];
